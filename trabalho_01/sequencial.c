3f299be12e1d9155c8057cfef264af23b52c6ae9
Beatriz de Oliveira Piedade <beatrizopiedade@outlook.com>
2025-10-12 20:40:56 -0300

    .

---
+++ trabalho_01/sequencial.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <time.h>
+
+#define TAM 1000
+
+int main () {
+    clock_t tempo_final, tempo_inicial = clock();
+    double tempo_total;
+
+    int x, soma = 0, subtracao = 0;
+    long double multiplicacao = 1;
+    int vet[TAM];
+
+    for (x = 0; x < TAM; x++) {
+        vet[x] = x + 1;
+    }
+
+    for (x = 0; x < TAM; x++) {
+        printf("vet[%d] = %d\n", x, vet[x]);
+    }
+
+    for (x = 0; x < TAM; x++) {
+        soma = soma + vet[x];
+        subtracao = subtracao - vet[x];
+        multiplicacao = multiplicacao * vet[x];
+    }
+    
+    tempo_final = clock();
+    tempo_total = (((double)tempo_final - (double)tempo_inicial) / CLOCKS_PER_SEC);
+    printf("Soma = %d\n", soma);
+    printf("Subtracao = %d\n", subtracao);
+    printf("Multiplicacao = %Lf\n", multiplicacao);
+	printf("TEMPO TOTAL: %.6fs\n", tempo_total);
+}
